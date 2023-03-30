918f17824526897b3a2c0952579947c202b62ae5
Stan <stan@stansa.dev>
2023-03-30 13:12:37 -0700

    Move loops to s3

--- server.py
+++ server.py
@@ -2,22 +2,35 @@
 
 import random
 
-from flask import Flask, send_from_directory
+import boto3
+from flask import Flask
 from flask_cors import CORS
-
-from data import loop_filename_from_info, get_comb_loop_infos_from_folder
 
 app = Flask(__name__)
 
 CORS(app)
 
+s3 = boto3.client('s3')
+
+S3_BACKEND_HOST = 'https://loopme-comb-loops.s3.us-west-1.amazonaws.com/'
+
+def info_from_filename(fn):
+    retval = dict()
+    parts = fn.split('_')
+    parts[-1] = parts[-1].split('.')[0]
+    retval['name'] = parts[0]
+    retval['tempo'] = parts[1]
+    retval['key'] = parts[2]
+    return retval
+
+def choose_random_s3_file():
+    res = s3.list_objects_v2(Bucket='loopme-comb-loops').get('Contents')
+    fn_list = [ obj.get('Key') for obj in res ]
+    return random.choice(fn_list)
+
 @app.route('/loop')
 def get_loop():
-    loop_infos = get_comb_loop_infos_from_folder()
-    loop_info = random.choice(loop_infos)
-    return {'url': '/combined_loops/{}'.format(loop_filename_from_info(loop_info)),
+    fn = choose_random_s3_file()
+    loop_info = info_from_filename(fn)
+    return {'url': S3_BACKEND_HOST + fn,
             'info': loop_info}
-
-@app.route('/combined_loops/<path:path>')
-def get_a_loop(path):
-    return send_from_directory('combined_loops', path)
